24b056acdc6dea7663b99045ff4cd6e7c82c7e98
David Lu!! <davidvlu@gmail.com>
2014-03-08 15:01:19 -0600

    Working scorer server

--- src/trajectory_scorer.cpp
+++ src/trajectory_scorer.cpp
@@ -1,28 +1,63 @@
 #include <dwa_local_planner/dwa_planner_ros.h>
+#include <std_srvs/Empty.h>
+#include<trajectory_scorer/ScoreTrajectory.h>
+
+class Scorer {
+    public:
+    Scorer() : planner(NULL), costmap(NULL) {
+        ros::NodeHandle nh("~");
+        a = nh.advertiseService("reset", &Scorer::callback, this);
+        b = nh.advertiseService("score", &Scorer::score, this);
+        reset();
+    }
+    
+    
+    bool callback(std_srvs::Empty::Request& request, std_srvs::Empty::Response& response)
+    {
+        reset();
+        return true;
+    }
+    
+    bool score(trajectory_scorer::ScoreTrajectory::Request& r, 
+               trajectory_scorer::ScoreTrajectory::Response& request)
+    {
+        
+        double s = planner->scoreTrajectory(r.x,0,0,0,0,0,r.cvx, 0,0);
+        ROS_INFO("Score: %f", s);
+        request.score = s;
+        return true;
+    }
+    
+    private:
+        void reset(){ 
+            if(planner) delete planner;
+            if(costmap) delete costmap;
+            
+            planner = new dwa_local_planner::DWAPlannerROS();
+            costmap = new costmap_2d::Costmap2DROS("local", tf);
+            planner->initialize("planner", &tf, costmap);   
+        }
+        tf::TransformListener tf;
+        
+        
+    dwa_local_planner::DWAPlannerROS* planner;
+    costmap_2d::Costmap2DROS* costmap;
+    ros::ServiceServer a,b;
+};
+
 
 int main(int argc, char** argv)
 {
-    ros::init(argc, argv, "standalone");
+    ros::init(argc, argv, "trajectory_scorer");
     
-    double x = 2, y = 0;
-    bool loop = false;
-    ROS_INFO("%d", argc);
+    /*double x = 2, y = 0;
     if(argc>1){
         if(argc>2){
             x = atof(argv[1]);
             y = atof(argv[2]);
         }
-        if(strcmp(argv[argc-1], "-l")==0){
-            loop = true;
-        }
     }
-
-    tf::TransformListener tf;
     
-    dwa_local_planner::DWAPlannerROS planner;
-    costmap_2d::Costmap2DROS costmap("local", tf);
-    planner.initialize("planner", &tf, &costmap);   
-
     std::vector<geometry_msgs::PoseStamped> plan;
     int N = 100;
     for(int i=0;i<N;i++){
@@ -33,13 +68,10 @@ int main(int argc, char** argv)
         pose.pose.position.y = i * y / N;
         plan.push_back(pose);
     }
-    planner.setPlan(plan);
+    //planner.setPlan(plan);*/
+    Scorer s;
+    ros::spin();
 
-    for(double i=0.0;i<=2;i+=.1){
-    for(double v=0.0;v<=.5;v+=.1)
-        printf("%f ", planner.scoreTrajectory(i,0,0,0,0,0,v, 0,0));
-        printf("\n");   
-    }
 
     return 0;
 }
